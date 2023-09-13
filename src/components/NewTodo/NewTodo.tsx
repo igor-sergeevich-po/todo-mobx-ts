195b867eff2f9ba97732c2b1974cac63bd581a7b
Igor_PO <89515076779@ya.ru>
2023-09-13 18:11:37 +0300

    add: logic add new todo

--- src/components/NewTodo/NewTodo.tsx
+++ src/components/NewTodo/NewTodo.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+import TodosStore from '../../store/todo-store'
+import { v4 as uuid } from 'uuid'
 import './style.css';
 
 const NewTodo = () => {
+	const [titleTodo, setTitleTodo] = useState('');
+	const { addTodo } = TodosStore;
+	const todo = {
+		id: uuid(),
+		name: titleTodo,
+		description: 'описание задачи',
+		isComplete: false
+	};
+
+	function addTodoItem() {
+		if (titleTodo.length > 5) {
+
+			addTodo(todo);
+			setTitleTodo('');
+		} else {
+			alert('слишком короткое название, введите больше 5 символов')
+		}
+	}
 	return (
 		<form action='#' className='newTodo'>
-			<input type="text" placeholder='текст новой задачи' />
-			<input type='submit' value='Добавить' />
+			<input onChange={(evt) => setTitleTodo(evt.target.value)} type="text" placeholder='текст новой задачи' value={titleTodo} />
+			<input onClick={() => addTodoItem()} type='submit' value='Добавить' />
 		</form>
 	);
 };
